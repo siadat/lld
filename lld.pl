7baeb8ce4c59a24bcbd4889b65ac1484eef0acbd
Sina Siadat <siadat@gmail.com>
2011-12-08 02:44:18 +0330

    less code + removed @new_words.

--- lld.pl
+++ lld.pl
@@ -6,29 +6,25 @@ use Term::ANSIColor;
 use Algorithm::Diff;
 
 my @prev_words = ();
-my @new_words = my @words = [];
+my @words = [];
 
 while(<>) {
   $_ = Term::ANSIColor::colorstrip($_);
   utf8::decode($_);
-  @new_words = @words = split(/(\W)/, $_);
+  @words = split(/(\W)/, $_);
   my @hunks = Algorithm::Diff::diff(\@prev_words, \@words);
+  @prev_words = @words;
 
   foreach my $hunk (@hunks) {
-    foreach my $diff (@{$hunk}) {
-      my @x = @{$diff};
-      my $operand = $x[0];
-      my $index = $x[1];
-      my $str = $x[2];
+    foreach my $diff (@$hunk) {
+      my ($operand, $index, $str) = @$diff;
       if ($operand eq "+") {
-        ${new_words[$index]} = colored($str, 'cyan');
+        $words[$index] = colored($str, 'cyan');
       }
     }
   }
 
-  my $newline = join('', @new_words);
+  my $newline = join('', @words);
   utf8::encode($newline);
   print $newline;
-
-  @prev_words = @words;
 }
